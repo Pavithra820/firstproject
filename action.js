8409804ca9c75810e918b7ca08ef37bf56bf2c98
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2021-05-17 22:14:11 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,13 @@
+function brown__btn(){
+    document.body.style.background="brown"
+   }
+   
+   function pink__btn()
+   {
+       document.body.style.background="pink"
+   }
+   
+   function green__btn(){
+       document.body.style.background="green"
+   }
+   
